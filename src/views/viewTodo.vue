eedfff275d49913f2ea000de8e09c41d963b8f8a
Moenul <moenul22@gmail.com>
2024-07-15 22:50:47 +0600

    User info nav added

--- src/views/viewTodo.vue
+++ src/views/viewTodo.vue
@@ -2,6 +2,44 @@
   <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6">
+      <!-- Navigation -->
+      <div class="nav d-block card p-3 m-4 mt-5">
+        <div class="use_info w-75 d-inline-block">
+          <div class="row">
+            <div class="col-2">
+              <img class="rounded-circle border border-info shadow-sm" style="width: 54px; overflow: hidden;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlCLUUa5t9Q5fC3sR8353U21-qY5qUt0i_pA&s" alt="">
+            </div>
+            <div class="col-10">
+              <p class="m-0">User Full Name</p>
+              <span class="text-muted" style="font-size: 16px;">moenul22@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="button text-danger m-0">
+          <iconify-icon icon="lets-icons:on-button-duotone"></iconify-icon>
+        </div>
+
+        <div class="todo_info d-block mt-2">
+          <div class="row">
+            <div class="col-2 d-inline-flex"><iconify-icon class="text-success" icon="weui:done2-filled" style="font-size: 23px; margin-right: 4px;"></iconify-icon> 10</div>
+            <div class="col-2 d-inline-flex"><iconify-icon class="text-warning" icon="fluent:calendar-clock-16-filled" style="font-size: 23px; margin-right: 4px;"></iconify-icon> 5</div>
+            <div class="col-8 d-inline-flex"><iconify-icon class="text-primary" icon="majesticons:clock" style="font-size: 23px; margin-right: 4px;"></iconify-icon> 25 Jul 2024</div>
+          </div>
+        </div>
+
+        
+      </div>
+      
+      <!-- if not sign in -->
+      <!-- <div class="nav d-block card p-3 m-4 mt-5">
+        <span class="title">SignIn to add task</span>
+        <div class="button text-info">
+          <iconify-icon icon="lets-icons:on-button-duotone"></iconify-icon>
+        </div>
+      </div> -->
+      <!-- if not sign in -->
+
+      <!-- Navigation -->
       <div class="todoContainer card p-3 m-4 mt-5">
         <!-- Header -->
         <div class="head text-center text-info">
@@ -200,4 +238,25 @@ const clearToDos = () => {
   visibility: visible;
   opacity: 1;
 }
+/* Nav section */
+.nav{
+  border-radius: 15px;
+    border: 1px solid #e4e4e4;
+    box-shadow: 0 0 0 0.2rem rgb(214 214 214 / 17%);
+}
+.nav .todo_info{
+  font-size: 16px;
+}
+.nav .title{
+  width: 85%;
+  display: inline-block;
+}
+.nav .button{
+  width: 40px;
+  float: right;
+  font-size: 38px;
+  margin-top: -10px;
+  cursor: pointer;
+}
+
 </style>
